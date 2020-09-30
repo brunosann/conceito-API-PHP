dd286f956d879fef819ded2662c6e9bc78bd8ffe
Bruno Teixeira <bruno_teixeira@hotmail.com>
2020-09-29 21:58:32 -0300

    .

--- api/return.php
+++ api/return.php
@@ -1,5 +1,6 @@
 <?php
 header("HTTP/1.0 200 "); // defino e status code da resposta
+// http_response_code(404);
 header("Access-Control-Allow-Origin: *"); //permito os ip que poder fazer uma requisião
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); //defino os métodos que eu aceito 
 header("Content-Type: application/json"); //ja defino que que meu retorno é um JSON
